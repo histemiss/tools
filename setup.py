5acefb29557872580006778a07f7744f4f769003
sword <histemiss@163.com>
2014-02-17 23:23:36 +0800

    设置程序icon

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 from distutils.core import setup
 import py2exe
 
-setup(data_files = [('res', ['res/checked.ico', 'res/notchecked.ico'])],
-      windows=['ntoq_gui.py'],
-      options = {
-          "py2exe": {"dll_excludes":["MSVCP90.dll"]}
-          }
-      )
+setup(
+    data_files = [('res', ['res/checked.ico', 'res/notchecked.ico'])],
+    windows =[{
+        "script":'ntoq_gui.py',
+        "icon_resources":[
+            (1, "res/test.ico")
+        ]}],
+    options = {
+        "py2exe": {
+            "dll_excludes":["MSVCP90.dll"],
+#            "bundle_files":1
+        }
+    }
+)
 
